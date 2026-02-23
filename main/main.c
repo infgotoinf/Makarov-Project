34639e3c30ea4516fff8f9d14e2e417223434d97
-inf <infgotoinf@gmail.com>
2026-02-23 12:40:16 +0300

    Messing around with raylib

--- main/main.c
+++ main/main.c
@@ -11,6 +11,16 @@ int main(void)
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    HideCursor();
+    
+    Vector2 pos;
+
+    Camera3D camera = { 0 };
+    camera.position = (Vector3){ 0.0f, 10.0f, 10.0f };  // Camera position
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 45.0f;                                // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -19,20 +29,52 @@ int main(void)
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        //---------------------------------------------------------------------------------
 
+        if (IsKeyDown(KEY_S))
+        {
+            camera.position.y -= 1;
+        }
+
+        if (IsKeyDown(KEY_W))
+        {
+            camera.position.y += 1;
+        }
+
+        if (IsKeyDown(KEY_A))
+        {
+            camera.position.x -= 1;
+        }
+
+        if (IsKeyDown(KEY_D))
+        {
+            camera.position.x += 1;
+        }
+        
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            BeginMode3D(camera);
+            
+                static Vector3 cubePosition = { 0.0f, 0.0f, 0.0f };
+                DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
+                DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
+
+                DrawGrid(10, 1.0f);
+
+            EndMode3D();
+
+            // DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+            pos = GetMousePosition();
+            static float size = 5.0f;
+            DrawRectangle(pos.x, pos.y, size-1, size-1, BLACK);
+            DrawRectangleLines(pos.x, pos.y, size, size, WHITE);
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
     // De-Initialization
