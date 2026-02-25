53a342dd637d085cac10bd3ee86581f45fd5b6b0
-inf <infgotoinf@gmail.com>
2026-02-25 20:58:58 +0000

    Fix ingame movement, move code that renders main menu to main/main_menu.c, add main/include/window_codes.h, make naming style consistent.

---
+++ main/include/main_menu.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_MENU
+#define MAIN_MENU
+
+// Returns exit code dependig on pressed button
+int DrawMainMenu();
+
+#endif // MAIN_MENU
---
+++ main/include/window_codes.h
@@ -0,0 +1,9 @@
+#ifndef WINDOW_CODES
+#define WINDOW_CODES
+
+// I thought about using enums, but raylib uses macros in it's API
+// and macros looks kinda nicer
+#define MAIN_MENU_CODE 0
+#define GAME_CODE 1
+
+#endif // WINDOW_CODES
